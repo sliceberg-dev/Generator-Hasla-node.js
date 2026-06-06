56b8bef5c228a90c2362e112e26f819da06a4387
sliceberg-dev <sliceberg915@gmail.com>
2026-06-06 20:17:28 +0200

    Delete generatorhasla.js

--- generatorhasla.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function generatePassword(length = 12) {
-  return crypto.randomBytes(length)
-    .toString('base64')
-    .slice(0, length);
-}
-
-console.log(generatePassword(16));
